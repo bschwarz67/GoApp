33e42d29e7b4bf4bc04a0e3254b2c91c1b7a0242
bschwarz67 <bryan.schwarz564@gmail.com>
2022-08-22 22:40:30 -0500

    make sure to import HttpResponsePermanentRedirect

--- goApp/goApp/views.py
+++ goApp/goApp/views.py
@@ -1,5 +1,5 @@
 from django.contrib import auth
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponsePermanentRedirect
 from django.urls import reverse
 
 
